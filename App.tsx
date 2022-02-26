2c80b442ca167bf1a59ffd55f7b040fd6b329cb8
Freddie John Millman <freddie.millman@icloud.com>
2022-02-26 19:15:49 +0000

    Add storybook for react native to run through expo

--- App.tsx
+++ App.tsx
@@ -1,1 +1,9 @@
-export { default } from './src/screens/HomeScreen'
+import { FC } from 'react'
+import Constants from 'expo-constants';
+
+import Storybook from './.storybook/react-native';
+import HomeScreen from './src/screens/HomeScreen';
+
+const App: FC = () => !!Constants.manifest?.extra?.IS_STORYBOOK ? <Storybook /> : <HomeScreen />;
+
+export default App;
---
+++ metro.config.js
@@ -0,0 +1,20 @@
+const { getDefaultConfig } = require('expo/metro-config');
+
+const defaultConfig = getDefaultConfig(__dirname);
+
+defaultConfig.resolver.resolverMainFields = [
+  'sbmodern',
+  'react-native',
+  ...defaultConfig.resolver.resolverMainFields,
+];
+
+defaultConfig.transformer.getTransformOptions = async () => ({
+  transform: {
+    experimentalImportSupport: false,
+    inlineRequires: false,
+  },
+});
+
+defaultConfig.watchFolders = [...defaultConfig.watchFolders, './.storybook/react-native'];
+
+module.exports = defaultConfig;
