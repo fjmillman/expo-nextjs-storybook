183898aeac95630dbf72b847a9cbc1a6928aebe6
Freddie John Millman <freddie.millman@icloud.com>
2022-02-26 15:40:11 +0000

    Convert to Typescript and add editorconfig

--- babel.config.js
+++ babel.config.js
@@ -2,7 +2,7 @@ module.exports = (api) => {
   api.cache(true)
 
   return {
-    presets: ['@expo/next-adapter/babel'],
+    presets: ['@expo/next-adapter/babel', ['babel-preset-expo', { web: { useTransformReactJSXExperimental: true } }]],
     plugins: [
       ['@babel/plugin-proposal-class-properties', { loose: true }],
       ['@babel/plugin-proposal-private-methods', { loose: true }],
