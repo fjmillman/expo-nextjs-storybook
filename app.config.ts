96355eeff77ea2be51c93b7388c8e33a925ab90e
Freddie John Millman <freddie.millman@icloud.com>
2022-02-27 14:49:01 +0000

    Apply universe eslint

--- app.config.ts
+++ app.config.ts
@@ -1,8 +1,8 @@
-import { ExpoConfig } from '@expo/config-types';
+import { ExpoConfig } from "@expo/config-types";
 
 const config: ExpoConfig = {
-  name: 'Expo Next.js Storybook',
-  slug: 'expo-nextjs-storybook',
+  name: "Expo Next.js Storybook",
+  slug: "expo-nextjs-storybook",
   extra: {
     IS_STORYBOOK: process.env.IS_STORYBOOK,
   },
--- babel.config.js
+++ babel.config.js
@@ -2,25 +2,25 @@ module.exports = (api) => {
   const isWeb = api.caller(
     (caller) =>
       caller &&
-      (caller.name === 'babel-loader' ||
-        caller.name === 'next-babel-turbo-loader'),
+      (caller.name === "babel-loader" ||
+        caller.name === "next-babel-turbo-loader")
   );
-  const isStorybook = api.env('storybook');
+  const isStorybook = api.env("storybook");
 
   return {
     presets: [
       isWeb &&
         !isStorybook && [
-          'next/babel',
-          { 'preset-env': { loose: true, shippedProposals: true } },
+          "next/babel",
+          { "preset-env": { loose: true, shippedProposals: true } },
         ],
       [
-        'babel-preset-expo',
+        "babel-preset-expo",
         { web: { useTransformReactJSXExperimental: true } },
       ],
     ].filter(Boolean),
     plugins: [
-      isStorybook && ['@babel/plugin-proposal-class-properties'],
+      isStorybook && ["@babel/plugin-proposal-class-properties"],
     ].filter(Boolean),
   };
 };
