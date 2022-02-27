21038c99b0ac63ff033884a5eb0d7de97220515f
Freddie John Millman <freddie.millman@icloud.com>
2022-02-27 01:04:57 +0000

    Ensure babel config works for storybook and next

--- babel.config.js
+++ babel.config.js
@@ -1,25 +1,12 @@
 module.exports = (api) => {
-  if (api.env("storybook")){
-    return {
-      presets: ["@babel/preset-env", "@babel/preset-react"],
-      plugins: ["@babel/plugin-proposal-class-properties"]
-    }
-  }
-
   const isWeb = api.caller((caller) => caller && (caller.name === 'babel-loader' || caller.name === 'next-babel-turbo-loader'));
-
-  if (!isWeb) {
-    return {
-      presets: [
-        [require.resolve('babel-preset-expo'), { web: { useTransformReactJSXExperimental: true } }],
-      ]
-    }
-  }
+  const isStorybook = api.env("storybook");
 
   return {
     presets: [
-      [require.resolve('next/babel'), { 'preset-env': { loose: true, shippedProposals: true } }],
-      [require.resolve('babel-preset-expo'), { web: { useTransformReactJSXExperimental: true } }],
-    ],
-  }
+      isWeb && !isStorybook && ['next/babel', { 'preset-env': { loose: true, shippedProposals: true } }],
+      ['babel-preset-expo', { web: { useTransformReactJSXExperimental: true } }],
+    ].filter(Boolean),
+    plugins: [isStorybook && ["@babel/plugin-proposal-class-properties"]].filter(Boolean),
+  };
 }
