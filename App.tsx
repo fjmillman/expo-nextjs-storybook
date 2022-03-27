72d38c1150a61c5eb88771e2575aea7500784558
Freddie John Millman <freddie.millman@icloud.com>
2022-03-27 13:13:17 +0100

    Clean up storybook commands and update tests to use chromatic instance for reference

--- App.tsx
+++ App.tsx
@@ -1,10 +1,6 @@
 import Constants from 'expo-constants';
-import { FC } from 'react';
 
 import Storybook from './.storybook/react-native';
 import HomeScreen from './src/screens/HomeScreen';
 
-const App: FC = () =>
-  Constants.manifest?.extra?.IS_STORYBOOK ? <Storybook /> : <HomeScreen />;
-
-export default App;
+export default Constants.manifest?.extra?.IS_STORYBOOK ? Storybook : HomeScreen;
